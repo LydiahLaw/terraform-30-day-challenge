94a4cbba316aa712687ec7b89177b75bc8aecd2c
LydiahLaw <Lygashiku@gmail.com>
2026-03-21 16:41:26 +0300

    add day 5 folder

---
+++ Day-Five/main.tf
@@ -0,0 +1,133 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_availability_zones" "all" {}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_security_group" "instance_sg" {
+  name        = "terraform-day05-instance-sg"
+  description = "Allow HTTP traffic to instances"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "terraform-day05-alb-sg"
+  description = "Allow HTTP traffic to ALB"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_launch_template" "web" {
+  name_prefix   = "terraform-day05-"
+  image_id      = "ami-0cebfb1f908092578"
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y apache2
+              systemctl start apache2
+              systemctl enable apache2
+              echo "<h1>Hello from Terraform - Day 05 Cluster</h1>" > /var/www/html/index.html
+              EOF
+  )
+}
+
+resource "aws_autoscaling_group" "web" {
+  name                = "terraform-day05-asg"
+  vpc_zone_identifier = data.aws_subnets.default.ids
+  target_group_arns   = [aws_lb_target_group.web.arn]
+  health_check_type   = "ELB"
+
+  min_size = 2
+  max_size = 5
+
+  launch_template {
+    id      = aws_launch_template.web.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-day05-asg-instance"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "web" {
+  name               = "terraform-day05-alb"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb_sg.id]
+}
+
+resource "aws_lb_target_group" "web" {
+  name     = "terraform-day05-tg"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.web.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web.arn
+  }
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.web.dns_name
+  description = "DNS name of the Application Load Balancer"
+}
---
+++ Day-Five/variables.tf
@@ -0,0 +1,23 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 80
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "server_name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+  default     = "terraform-day05-server"
+}
