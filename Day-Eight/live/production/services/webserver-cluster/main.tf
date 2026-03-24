bfae02bfde51ec9b8a453fae50b876d7653c64a3
LydiahLaw <Lygashiku@gmail.com>
2026-03-25 00:11:08 +0300

    day 8

---
+++ Day-Eight/live/production/services/webserver-cluster/main.tf
@@ -0,0 +1,8 @@
+module "webserver_cluster" {
+  source = "../../../../modules/services/webserver-cluster"
+
+  cluster_name  = "webservers-production"
+  instance_type = "t2.medium"
+  min_size      = 4
+  max_size      = 10
+}
